ab7097fa57ce7ca80b999b104e3160b0317e20d7
ilya9293 <ilya9293@ukr.net>
2021-12-25 16:22:53 +0200

    2 - е задание

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const list = document.querySelector('#ingredients');
+const elem = [];
+
+ingredients.forEach(ingrd => {
+  const item = document.createElement('li');
+  item.textContent = ingrd;
+  elem.push(item);
+  list.append(...elem);
+});
